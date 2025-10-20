f596d595eafa9874cb1e212dc4daa449dbbfe64c
Mark Emery <mark@markemerylimited.co.uk>
2025-10-20 10:28:39 +0100

    Create five-star.pl

---
+++ five-star.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+$server='https://<IP-address.hexstring.plex.direct:<port>';
+$client='<USERNAME>';
+
+$sessions=`curl -o - -s "$server/status/sessions"`;
+$sessions=~s/\n//g;
+$sessions=~s/\r//g;
+$sessions=~/^.*(Track.*?$client.*?Track).*$/;
+$session=$1;
+if ($session) {
+   $session=~/^.*?title="(.*?)"/;
+   $title=$1;
+   $session=~/^.*? parentTitle="(.*?)"/;
+   $parentTitle=$1;
+   $session=~/^.*? originalTitle="(.*?)"/;
+   $originalTitle=$1;
+   $session=~/^.*? ratingKey="(.*?)"/;
+   $ratingKey=$1;
+   $res=`curl -s -X PUT "$server/:/rate?identifier=com.plexapp.plugins.library&key=$ratingKey&rating=10"`;
+   $title=~s/&#8217;/'/g;
+   $title=~s/&#39;/'/g;
+   print "Rated ". $originalTitle ."'s track $title as five star";
+}
