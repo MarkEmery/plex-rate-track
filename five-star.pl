adad03d50d3f7302fca3c13a8d6df89d900369f9
Mark Emery <mark@markemerylimited.co.uk>
2025-10-20 22:09:52 +0100

    Update five-star.pl

--- five-star.pl
+++ five-star.pl
@@ -2,6 +2,8 @@
 
 $server='https://<IP-address.hexstring>.plex.direct:<port>';
 $client='<USERNAME>';
+$rating_Num=10;
+$rating_Word='five';
 
 $sessions=`curl -o - -s "$server/status/sessions"`;
 $sessions=~s/\n//g;
@@ -11,16 +13,17 @@ $session=$1;
 if ($session) {
    $session=~/^.*?title="(.*?)"/;
    $title=$1;
-   $session=~/^.*? parentTitle="(.*?)"/;
-   $parentTitle=$1;
+   $session=~/^.*? grandparentTitle="(.*?)"/;
+   $grandparentTitle=$1;
    $session=~/^.*? originalTitle="(.*?)"/;
    $originalTitle=$1;
    $session=~/^.*? ratingKey="(.*?)"/;
    $ratingKey=$1;
-   $res=`curl -s -X PUT "$server/:/rate?identifier=com.plexapp.plugins.library&key=$ratingKey&rating=10"`;
+   $res=`curl -s -X PUT "$server/:/rate?identifier=com.plexapp.plugins.library&key=$ratingKey&rating=$rating_Num"`;
    $title=~s/&#8217;/'/g;
    $title=~s/&#39;/'/g;
-   print "Rated ". $originalTitle ."'s track $title as five star";
+   print     "Rated ". $grandparentTitle ."'s track ". $title ." as ". $rating_Word. " star\n";
+   print LOG "Rated ". $grandparentTitle ."'s track ". $title ." as ". $rating_Word. " star\n";
 } else {
    print "Sorry, $client, I didn't find anything playing.\n";
 }
